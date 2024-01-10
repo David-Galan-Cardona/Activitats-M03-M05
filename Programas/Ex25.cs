69dfbc2072d3a2073ec43afa0fda5415582f0606
David-Galan-Cardona <147103423+David-Galan-Cardona@users.noreply.github.com>
2024-01-10 12:43:02 +0100

    Add files via upload

---
+++ Programas/Ex25.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Recursividad
+{
+    class Ex25
+    {
+        public static void Main()
+        {
+            int num;
+            int div = 2;
+
+            Console.WriteLine("Introdueix el nombre");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("El numero {0} en binari és {1}", num, DecimalToBinary(num));
+        }
+        public static int DecimalToBinary(int num)
+        {
+            if (0 == num)
+            {
+                return 0;
+            }
+            return num%2 + 10 * DecimalToBinary(num/2);
+        }
+    }
+}
