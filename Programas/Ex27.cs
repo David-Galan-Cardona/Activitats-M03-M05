5dad7f8f7839fda1729be5b2b442dfd59d5faeed
David-Galan-Cardona <147103423+David-Galan-Cardona@users.noreply.github.com>
2024-01-10 12:30:39 +0100

    Add files via upload

---
+++ Programas/Ex27.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Recursividad
+{
+    class Ex27
+    {
+        public static void Main()
+        {
+            int num;
+            int div = 2;
+
+            Console.WriteLine("Introdueix el nombre");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num != 1) Console.WriteLine(Prime(num, div) ? "Es primer" : "No es primer");
+            else Console.WriteLine("Es primer");
+        }
+        public static bool Prime(int num, int div)
+        {
+            if (div==num)
+            {
+                return true;
+            }
+            else if (num%div == 0)
+            {
+                return false;
+            }
+            return Prime(num, div+1);
+        }
+    }
+}
